288364f37a702679d25e933886f1420953ec230f
lvo711717 <lvo711717@gmail.com>
2024-04-05 12:27:42 +0300

    checking deployment

--- CLIENT/src/features/home/HomePage.tsx
+++ CLIENT/src/features/home/HomePage.tsx
@@ -2,6 +2,6 @@ import { Typography } from "@mui/material";
 
 export default function HomePage(){
     return(
-        <Typography variant="h2">Home page</Typography>
+        <Typography variant="h2">Home page published version</Typography>
     )
 }
